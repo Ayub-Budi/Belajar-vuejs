e68f7c03c98fee04a25b18c3bcbf2e9daa141ca0
yubzz09 <ayubbudie02@gmail.com>
2024-06-01 21:49:59 +0700

    start project

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+
+  <Logos/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Logos from './components/Logos.vue'
+
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    Logos
   }
 }
 </script>
---
+++ src/components/Logos.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="logos-wrapper">
+        <h2 class="title-header">Find Best Logos</h2>
+        <div class="item-logos">
+            <img class="logo" alt="google" src="../assets/google.png">
+        </div>
+
+        <div class="item-logos">
+            <img class="logo" alt="gmail" src="../assets/gmail.png">
+        </div>
+
+        <div class="item-logos">
+            <img class="logo" alt="facebook" src="../assets/facebook.png">
+        </div>
+    </div>
+
+
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Logos'
+}
+</script>
+
+<style scoped>
+
+.title-header{
+    margin-bottom: 80px;
+}
+
+.logos-wrapper{
+    width: 70%;
+    overflow: hidden;
+    margin: auto;
+}
+
+.item-logos{
+    float: left;
+    margin-right: 20px;
+    margin-bottom: 20px;
+    width: 150px;
+    height: 150px;
+}
+
+.item-logos .logo{
+    width: 150px;
+    height: 150px;
+}
+
+</style>
