d7661d7b1398c9cbe2440c1f254b0fc8b603ef99
yubzz09 <ayubbudie02@gmail.com>
2024-06-02 08:35:33 +0700

    mengatur layout:

--- src/components/Logos.vue
+++ src/components/Logos.vue
@@ -1,52 +1,73 @@
 <template>
-    <div class="logos-wrapper">
-        <h2 class="title-header">Find Best Logos</h2>
-        <div class="item-logos">
-            <img class="logo" alt="google" src="../assets/google.png">
-        </div>
-
-        <div class="item-logos">
-            <img class="logo" alt="gmail" src="../assets/gmail.png">
-        </div>
-
-        <div class="item-logos">
-            <img class="logo" alt="facebook" src="../assets/facebook.png">
-        </div>
+  <div class="logos-wrapper">
+    <h2 class="title-header">Find Best Logos</h2>
+    <div class="item-logos">
+      <img class="logo" alt="google" src="../assets/google.png" />
     </div>
 
+    <div class="item-logos">
+      <img class="logo" alt="gmail" src="../assets/gmail.png" />
+    </div>
 
+    <div class="item-logos">
+      <img class="logo" alt="facebook" src="../assets/facebook.png" />
+    </div>
+
+    <div class="item-logos">
+      <img class="logo" alt="google" src="../assets/google.png" />
+    </div>
+
+    <div class="item-logos">
+      <img class="logo" alt="gmail" src="../assets/gmail.png" />
+    </div>
+
+    <div class="item-logos">
+      <img class="logo" alt="facebook" src="../assets/facebook.png" />
+    </div>
+
+    <div class="item-logos">
+      <img class="logo" alt="google" src="../assets/google.png" />
+    </div>
+
+    <div class="item-logos">
+      <img class="logo" alt="gmail" src="../assets/gmail.png" />
+    </div>
+
+    <div class="item-logos">
+      <img class="logo" alt="facebook" src="../assets/facebook.png" />
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
-  name: 'Logos'
-}
+  name: "Logos",
+};
 </script>
 
 <style scoped>
-
-.title-header{
-    margin-bottom: 80px;
+.title-header {
+  margin-bottom: 80px;
+  text-align: left;
 }
 
-.logos-wrapper{
-    width: 70%;
-    overflow: hidden;
-    margin: auto;
+.logos-wrapper {
+  width: 60%;
+  overflow: hidden;
+  margin: auto;
 }
 
-.item-logos{
-    float: left;
-    margin-right: 20px;
-    margin-bottom: 20px;
-    width: 150px;
-    height: 150px;
+.item-logos {
+  float: left;
+  margin-right: 70px;
+  margin-bottom: 70px;
+  width: 150px;
+  height: 150px;
 }
 
-.item-logos .logo{
-    width: 150px;
-    height: 150px;
+.item-logos .logo {
+  width: 150px;
+  height: 150px;
 }
-
 </style>
